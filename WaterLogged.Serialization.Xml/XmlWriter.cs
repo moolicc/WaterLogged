ace00a84500dba985bc52a03940fcc6623175382
Icecream-burglar <>
2017-09-04 17:46:52 -0500

    XmlWriter note added

---
+++ WaterLogged.Serialization.Xml/XmlWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace WaterLogged.Serialization.Xml
+{
+    public class XmlWriter
+    {
+        //TODO: Stop being lazy and finish me.
+    }
+}
