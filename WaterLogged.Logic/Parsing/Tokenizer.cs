f79e2a9d1b7d8de215f6b252c659605ad99af804
Icecream-burglar <>
2017-09-08 17:37:19 -0500

    Structure work

--- WaterLogged.Logic/Parsing/Tokenizer.cs
+++ WaterLogged.Logic/Parsing/Tokenizer.cs
@@ -6,8 +6,17 @@ namespace WaterLogged.Logic.Parsing
 {
     public class Tokenizer
     {
+        private string _expression;
+        private int _index;
+
         //$ means interpret as method
-        //# means interpret as number
+        //# means interpret as number (or bool)
         //% means interpret as string
+        //Nothing means interpret as string
+
+        public Tokens.Token[] EvaluateTokens(string expression)
+        {
+            
+        }
     }
 }
