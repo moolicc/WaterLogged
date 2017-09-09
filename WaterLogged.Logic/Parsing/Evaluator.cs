fe599279b5dca4e2057475d223c683a8c02a9d7c
Icecream-burglar <>
2017-09-09 17:04:12 -0500

    Renamed Parser to Evaluator and did some small structure work

---
+++ WaterLogged.Logic/Parsing/Evaluator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WaterLogged.Logic.Parsing
+{
+    public class Evaluator
+    {
+        public Context Context { get; set; }
+        public string Source { get; private set; }
+        public Tokenizer Tokenizer { get; private set; }
+
+        public Evaluator(string source)
+        {
+            Context = new Context();
+            Source = source;
+            Tokenizer = new Tokenizer();
+        }
+
+        public string Evaluate()
+        {
+            var tokens = Tokenizer.EvaluateTokens(Source);
+            var result = new StringBuilder();
+
+            return result.ToString();
+        }
+    }
+}
