26d2c03341ba26090e5cc729a9c88404cb2ebece
Icecream-burglar <>
2017-09-10 22:52:17 -0500

    LogicalExpression can now try to resolve functions used directly within the expression text.

--- WaterLogged.Parsing/Expressions/LogicalExpression.cs
+++ WaterLogged.Parsing/Expressions/LogicalExpression.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace WaterLogged.Parsing.Expressions
@@ -15,7 +16,34 @@ namespace WaterLogged.Parsing.Expressions
 
         public string Eval(Evaluator evaluator)
         {
-            return new NCalc.Expression(NestedExpression.Eval(evaluator)).Evaluate().ToString();
+            var expression = new NCalc.Expression(NestedExpression.Eval(evaluator));
+            if (expression.HasErrors())
+            {
+                return "[ERROR]";
+            }
+            expression.EvaluateParameter += (name, args) =>
+            {
+                Delegate func = null;
+                try
+                {
+                    func = evaluator.Context.GetDelegate(name);
+                }
+                catch
+                {
+                    throw new InvalidOperationException("Invalid logical operation.");
+                }
+
+                if (func.GetMethodInfo().GetParameters().Length == 0)
+                {
+                    args.HasResult = true;
+                    args.Result = func.DynamicInvoke();
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid logical operation.");
+                }
+            };
+            return expression.Evaluate().ToString();
         }
     }
 }
