4f9f8b273159372adbf996e84cfe4a0e10f4b503
Icecream-burglar <>
2017-09-15 03:18:09 -0500

    Made abstract, OCD'ed the position of the properties within the source, made SetName work when not owned by a Log

--- WaterLogged/Listener.cs
+++ WaterLogged/Listener.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections.Generic;
+using WaterLogged.Templating;
 
 namespace WaterLogged
 {
-    public class Listener
+    public abstract class Listener
     {
-        public Log Log { get; internal set; }
-        public string Name { get; internal set; }
         public bool Enabled { get; set; }
         public string[] TagFilter { get; set; }
+        public Log Log { get; internal set; }
+        public string Name { get; internal set; }
         public Dictionary<string, string> FormatterArgs { get; private set; }
 
-        public Listener()
+        protected Listener()
         {
             Enabled = true;
             TagFilter = new string[0];
@@ -24,10 +25,9 @@ namespace WaterLogged
                 Log.ChangeListenerName(Name, newName);
                 Name = newName;
             }
+            Name = newName;
         }
 
-        public virtual void Write(string value, string tag)
-        {
-        }
+        public abstract void Write(string value, string tag);
     }
 }
