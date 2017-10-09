0cd2f9b23bbadceca1b05dda3147d4635d5f8ea8
Icecream-burglar <>
2017-10-09 01:10:08 -0500

    Added the TemplateRedirectSink back in

---
+++ WaterLogged/Templating/TemplateRedirectSink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WaterLogged.Templating
+{
+    /// <summary>
+    /// Implements a <see cref="TemplatedMessageSink"/> that renders a structured message and outputs it back to the owning log.
+    /// </summary>
+    /// <inheritdoc />
+    public class TemplateRedirectSink : TemplatedMessageSink
+    {
+        public override void ProcessMessage(StructuredMessage message, string tag)
+        {
+            Log.WriteTag(TemplateProcessor.BuildString(message), tag);
+        }
+    }
+}
