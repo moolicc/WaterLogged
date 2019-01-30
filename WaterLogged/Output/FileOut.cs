5a93ba7c716edfa0fa062e04a684f27cd5e97b91
jw <icecreamburglar@gmail.com>
2019-01-30 16:22:15 -0600

    FileTypeTransformers added

--- WaterLogged/Output/FileOut.cs
+++ WaterLogged/Output/FileOut.cs
@@ -1,11 +1,15 @@
 ﻿
+using WaterLogged.Templating;
+
 namespace WaterLogged.Output
 {
     /// <summary>
     /// Implements a listener which appends output to a file.
     /// </summary>
-    public class FileOut : Listener
+    public class FileOut : ListenerSink
     {
+        public FileTypeTransformer FileTypeTransformer { get; set; }
+
         /// <summary>
         /// The specified file to append log messages to.
         /// </summary>
@@ -15,16 +19,17 @@ namespace WaterLogged.Output
         /// Instantiates a new instance of the FileOut listener. By default; appends to "log.txt".
         /// </summary>
         public FileOut()
-            : this("log.txt")
+            : this("log.txt", FileTypeTransformers.TextTransformer)
         {
         }
 
         /// <summary>
         /// Instantiates a new instance of the FileOut listener. Appends to the file at the specified path.
         /// </summary>
-        public FileOut(string filePath)
+        public FileOut(string filePath, FileTypeTransformer transformer)
         {
             FilePath = filePath;
+            FileTypeTransformer = transformer;
         }
 
         /// <summary>
@@ -32,7 +37,12 @@ namespace WaterLogged.Output
         /// </summary>
         public override void Write(string value, string tag)
         {
-            System.IO.File.AppendAllText(FilePath, value);
+            System.IO.File.AppendAllText(FilePath, FileTypeTransformer(value));
+        }
+
+        public override void ProcessMessage(StructuredMessage message, string tag)
+        {
+            System.IO.File.AppendAllText(FilePath, FileTypeTransformer("", message));
         }
     }
 }
---
+++ WaterLogged/Output/FileTypeTransformers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WaterLogged.Templating;
+
+namespace WaterLogged.Output
+{
+    public delegate string FileTypeTransformer(string input = "", StructuredMessage? message = null);
+
+    public sealed class FileTypeTransformers
+    {
+        public static FileTypeTransformer TextTransformer = (s, m) =>
+        {
+            if (m.HasValue)
+            {
+                return TemplateProcessor.BuildString(m.Value);
+            }
+            return s;
+        };
+    }
+}
